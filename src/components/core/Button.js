2d48d2105d3a0c7bbc216d0c84c2d6108ff7dfc4
Haekang Jeon <>
2022-12-05 19:09:58 +0900

    Add : Button 컴포넌트 생성

---
+++ src/components/core/Button.js
@@ -0,0 +1,49 @@
+import styles from "./Button.module.scss";
+import cn from "classnames";
+
+import { useMemo } from "react";
+import { Link } from "react-router-dom";
+
+export const BUTTON_SIZE_LARGE = "large";
+export const BUTTON_SIZE_MEDIUM = "medium";
+export const BUTTON_SIZE_SMALL = "small";
+
+export const BUTTON_COLOR_BLUE = "blue";
+export const BUTTON_COLOR_WHITE = "white";
+
+const Button = (props) => {
+  const {
+    size = BUTTON_SIZE_MEDIUM,
+    color = BUTTON_COLOR_BLUE,
+    full,
+    to,
+  } = props;
+
+  const _props = useMemo(() => {
+    const __props = { ...props };
+
+    ["size", "color", "full"].forEach((key) => {
+      delete __props[key];
+    });
+
+    return __props;
+  }, [props]);
+
+  const Tag = useMemo(() => {
+    let component = "button";
+
+    if (to) {
+      component = Link;
+    }
+
+    return component;
+  }, [to]);
+
+  const className = useMemo(() => {
+    return cn(styles.button, styles[size], styles[color], full);
+  }, [color, full, size]);
+
+  return <Tag className={className} {..._props} />;
+};
+
+export default Button;
