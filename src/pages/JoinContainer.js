cda8dd4d049f3f5ccee781002af5085bc304437d
Haekang Jeon <>
2022-12-06 00:32:05 +0900

    Add : Join, Login Container View 완성

--- src/pages/JoinContainer.js
+++ src/pages/JoinContainer.js
@@ -1,7 +1,51 @@
-import "./JoinContainer.module.scss";
+import styles from "./JoinContainer.module.scss";
+
+import { useCallback } from "react";
+
+import useForm from "hooks/useForm";
+
+import TextInput from "components/common/TextInput";
+import Button from "components/core/Button";
+
+const INIT_FORM = {
+  id: "",
+  password: "",
+};
 
 const JoinContainer = () => {
-  return <div>회원가입</div>;
+  const { setValue, getData, validation } = useForm(INIT_FORM);
+
+  const requestJoin = useCallback((e) => {
+    e.preventDefault();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="common-title">회원가입</h1>
+
+      <form className={styles["join-form"]} onSubmit={requestJoin}>
+        <TextInput
+          type={"email"}
+          name={"id"}
+          message={"이메일 형식으로 입력해 주세요."}
+          title={"아이디"}
+          required
+          onChange={setValue}
+        />
+
+        <TextInput
+          type={"password"}
+          name={"password"}
+          message={"최소 8자 이상 입력해 주세요."}
+          title={"비밀번호"}
+          minLength={8}
+          required
+          onChange={setValue}
+        />
+        <Button disabled={!validation}>회원가입</Button>
+      </form>
+    </div>
+  );
 };
 
 export default JoinContainer;
--- src/pages/LoginContainer.js
+++ src/pages/LoginContainer.js
@@ -1,7 +1,48 @@
 import styles from "./LoginContainer.module.scss";
 
+import { useCallback } from "react";
+
+import useForm from "hooks/useForm";
+
+import TextInput from "components/common/TextInput";
+import Button from "components/core/Button";
+
+const INIT_FORM = {
+  id: "",
+  password: "",
+};
+
 const LoginContainer = () => {
-  return <div>로그인</div>;
+  const { setValue, getData, validation } = useForm(INIT_FORM);
+
+  const requestLogin = useCallback((e) => {
+    e.preventDefault();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="common-title">로그인</h1>
+
+      <form className={styles["login-form"]} onSubmit={requestLogin}>
+        <TextInput
+          type={"text"}
+          name={"id"}
+          title={"아이디"}
+          required
+          onChange={setValue}
+        />
+
+        <TextInput
+          type={"password"}
+          name={"password"}
+          title={"비밀번호"}
+          required
+          onChange={setValue}
+        />
+        <Button disabled={!validation}>로그인</Button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginContainer;
