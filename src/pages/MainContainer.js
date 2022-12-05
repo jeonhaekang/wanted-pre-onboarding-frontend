f5a4e5c6c607617ff930adcd62e0a0410734f23c
Haekang Jeon <>
2022-12-05 19:16:10 +0900

    Refactor : scss => module.scss 변경 및 버튼 연결

--- src/pages/MainContainer.js
+++ src/pages/MainContainer.js
@@ -1,7 +1,21 @@
-import "./MainContainer.scss";
+import styles from "./MainContainer.module.scss";
+
+import Button, { BUTTON_COLOR_WHITE } from "components/core/Button";
 
 const MainContainer = () => {
-  return <div>메인페이지</div>;
+  return (
+    <div className="common-container">
+      <h1 className="common-title">메인페이지</h1>
+
+      <div className={styles["button-box"]}>
+        <Button to={"login"}>로그인</Button>
+
+        <Button to={"join"} color={BUTTON_COLOR_WHITE}>
+          회원가입
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default MainContainer;
