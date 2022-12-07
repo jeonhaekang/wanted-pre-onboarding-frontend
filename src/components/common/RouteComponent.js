dcfb85c8b8bc26f6882a58bb77c4273f70541b61
Haekang Jeon <>
2022-12-07 17:24:29 +0900

    Refactor : 권한 체크 추가

--- src/components/common/RouteComponent.js
+++ src/components/common/RouteComponent.js
@@ -1,13 +1,37 @@
-import { useEffect } from "react";
+import { PERMISSION_ALL, PERMISSION_LOGIN } from "constants/permission";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const RouteComponent = (props) => {
-  const { title, component: Component } = props;
+  const { permission, title, component: Component } = props;
+
+  const navigate = useNavigate();
+
+  const [render, setRender] = useState(false);
 
   useEffect(() => {
     document.querySelector("title").innerHTML = title;
-  }, [title]);
 
-  return <Component />;
+    let passed = false;
+
+    if (permission === PERMISSION_ALL) {
+      passed = true;
+    } else if (permission === PERMISSION_LOGIN) {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+        passed = true;
+      }
+    }
+
+    if (!passed) {
+      navigate("/");
+    }
+
+    setRender(passed);
+  }, [navigate, permission, title]);
+
+  return render && <Component />;
 };
 
 export default RouteComponent;
