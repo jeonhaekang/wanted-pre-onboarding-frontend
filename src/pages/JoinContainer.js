a9df21fded7862f5ebdeb67c689f33d7d494c636
Haekang Jeon <>
2022-12-07 13:20:07 +0900

    Add : 로그인, 회원가입 Api 연결

--- src/pages/JoinContainer.js
+++ src/pages/JoinContainer.js
@@ -1,6 +1,9 @@
 import styles from "./JoinContainer.module.scss";
 
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+
+import AccountModel from "model/AccountModel";
 
 import useForm from "hooks/useForm";
 
@@ -8,16 +11,35 @@ import TextInput from "components/common/TextInput";
 import Button from "components/core/Button";
 
 const INIT_FORM = {
-  id: "",
+  email: "",
   password: "",
 };
 
 const JoinContainer = () => {
+  const navigate = useNavigate();
+
   const { setValue, getData, validation } = useForm(INIT_FORM);
 
-  const requestJoin = useCallback((e) => {
-    e.preventDefault();
-  }, []);
+  const account = useMemo(() => new AccountModel(), []);
+
+  const requestJoin = useCallback(
+    async (e) => {
+      e.preventDefault();
+      const response = await account.join(getData());
+
+      if (account.isSuccess(response)) {
+        localStorage.setItem("token", response.data.access_token);
+
+        alert("회원가입에 성공하였습니다.");
+        navigate("/");
+      }
+
+      if (account.isError(response)) {
+        alert(response.data.message);
+      }
+    },
+    [account, getData, navigate]
+  );
 
   return (
     <div>
@@ -26,7 +48,7 @@ const JoinContainer = () => {
       <form className={styles["join-form"]} onSubmit={requestJoin}>
         <TextInput
           type={"email"}
-          name={"id"}
+          name={"email"}
           message={"이메일 형식으로 입력해 주세요."}
           title={"아이디"}
           required
--- src/pages/LoginContainer.js
+++ src/pages/LoginContainer.js
@@ -1,23 +1,44 @@
 import styles from "./LoginContainer.module.scss";
 
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 
 import useForm from "hooks/useForm";
 
 import TextInput from "components/common/TextInput";
 import Button from "components/core/Button";
+import AccountModel from "model/AccountModel";
+import { useNavigate } from "react-router-dom";
 
 const INIT_FORM = {
-  id: "",
+  email: "",
   password: "",
 };
 
 const LoginContainer = () => {
+  const navigate = useNavigate();
+
   const { setValue, getData, validation } = useForm(INIT_FORM);
 
-  const requestLogin = useCallback((e) => {
-    e.preventDefault();
-  }, []);
+  const account = useMemo(() => new AccountModel(), []);
+
+  const requestLogin = useCallback(
+    async (e) => {
+      e.preventDefault();
+
+      const response = await account.login(getData());
+
+      if (account.isSuccess(response)) {
+        localStorage.setItem("token", response.data.access_token);
+
+        navigate("/");
+      }
+
+      if (account.isError(response)) {
+        alert("로그인 실패, 아이디나 비밀번호를 확인해 주세요");
+      }
+    },
+    [account, getData, navigate]
+  );
 
   return (
     <div>
@@ -26,7 +47,7 @@ const LoginContainer = () => {
       <form className={styles["login-form"]} onSubmit={requestLogin}>
         <TextInput
           type={"text"}
-          name={"id"}
+          name={"email"}
           title={"아이디"}
           required
           onChange={setValue}
